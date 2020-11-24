340b8ae6350632b182af77d27e4795614d8dfe5f
Dean Del Ponte <dean.delponte@gmail.com>
2020-11-24 18:39:55 +0000

    Initial commit

---
+++ src/test/groovy/com/genalpha/LambdaRuntimeBugSpec.groovy
@@ -0,0 +1,19 @@
+package com.genalpha
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import javax.inject.Inject
+
+@MicronautTest
+class LambdaRuntimeBugSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
